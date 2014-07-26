f0c6a7550340434c91ded36ce3a060388c037662
Tim Smith <tsmith@llnw.com>
2014-07-26 12:17:13 -0700

    Fix license in metadata

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,6 @@ name              'firefox'
 maintainer        'Tim Smith'
 maintainer_email  'tsmith@llnw.com'
 license           'Apache 2.0'
-license           'All rights reserved'
 description       'Installs Firefox for Windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '1.0.7'
