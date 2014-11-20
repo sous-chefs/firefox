e24a351bbea4b9ba8f591dd577b98a45f5a06ca9
Tim Smith <tim@cozy.co>
2014-11-20 14:36:29 -0800

    Update description

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              'firefox'
 maintainer        'Tim Smith'
 maintainer_email  'tsmith84@gmail.com'
 license           'Apache 2.0'
-description       'Installs Firefox'
+description       'Installs Mozilla Firefox on multiple operating systems'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '2.0.0'
 
