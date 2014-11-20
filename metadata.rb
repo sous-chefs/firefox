7d6927bf0349df33a2a60ac6367f723fa600dedf
Tim Smith <tsmith84@gmail.com>
2014-11-20 14:22:27 -0800

    Update my contact info

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name              'firefox'
 maintainer        'Tim Smith'
-maintainer_email  'tsmith@llnw.com'
+maintainer_email  'tsmith84@gmail.com'
 license           'Apache 2.0'
 description       'Installs Firefox'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
