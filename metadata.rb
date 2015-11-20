9f9118ecaa47bd67041ef26663e70af111dae639
Tim Smith <tsmith84@gmail.com>
2015-11-20 11:01:38 -0800

    Release 2.0.6

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tsmith84@gmail.com'
 license 'Apache 2.0'
 description 'Installs Mozilla Firefox on multiple operating systems'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.0.5'
+version '2.0.6'
 
 %w(redhat centos scientific amazon oracle windows mac_os_x debian ubuntu).each do |os|
   supports os
