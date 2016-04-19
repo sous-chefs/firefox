4d5226d7f51271c596df84a94cfdc5acf028dc16
Tim Smith <tsmith84@gmail.com>
2016-04-19 09:31:12 -0700

    Resolve rubocop warnings

--- test/fixtures/cookbooks/firefox_test/recipes/version.rb
+++ test/fixtures/cookbooks/firefox_test/recipes/version.rb
@@ -2,7 +2,7 @@ include_recipe 'firefox::default'
 
 version = firefox_version
 
-fail "Firefox version returned is invalid: #{version}" unless version.match(/[\d|.]*/)
+raise "Firefox version returned is invalid: #{version}" unless version =~ /[\d|.]*/
 
 log 'firefox version' do
   message version
