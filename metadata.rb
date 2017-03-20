f13eb5397544655bd18adf9867b1434cf91ab846
Tim Smith <tsmith@chef.io>
2017-03-20 14:40:20 -0700

    We actually require 12.6 because of windows_package
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'firefox'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs Mozilla Firefox on multiple operating systems'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '3.0.1'
@@ -14,4 +14,4 @@ depends 'dmg', '>= 2.2'
 
 source_url 'https://github.com/chef-cookbooks/firefox'
 issues_url 'https://github.com/chef-cookbooks/firefox/issues'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.6' if respond_to?(:chef_version)
