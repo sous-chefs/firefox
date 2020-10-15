4c4c0e7b863096f75af31dd6ce463c516b457519
Lance Albertson <lance@osuosl.org>
2020-10-15 15:00:09 -0700

    Update metadata.rb to Sous Chefs
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,19 @@
-name 'firefox'
-maintainer 'Chef Software, Inc.'
-maintainer_email 'cookbooks@chef.io'
-license 'Apache-2.0'
-description 'Installs Mozilla Firefox on multiple operating systems'
-version '6.0.0'
+name              'firefox'
+maintainer        'Sous Chefs'
+maintainer_email  'help@sous-chefs.org'
+license           'Apache-2.0'
+description       'Installs Mozilla Firefox on multiple operating systems'
+version           '6.0.0'
+source_url        'https://github.com/sous-chefs/firefox'
+issues_url        'https://github.com/sous-chefs/firefox/issues'
+chef_version      '>= 14'
 
-%w(redhat centos scientific amazon oracle windows mac_os_x debian ubuntu).each do |os|
-  supports os
-end
-
-source_url 'https://github.com/chef-cookbooks/firefox'
-issues_url 'https://github.com/chef-cookbooks/firefox/issues'
-chef_version '>= 14'
+supports 'amazon'
+supports 'centos'
+supports 'debian'
+supports 'mac_os_x'
+supports 'oracle'
+supports 'redhat'
+supports 'scientific'
+supports 'ubuntu'
+supports 'windows'
