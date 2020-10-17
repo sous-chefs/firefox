3aa2d95e91dc164cfd6b23287c428000c283907c
Lance Albertson <lance@osuosl.org>
2020-10-17 10:32:40 -0700

    Fix kitchen.yml for Windows/MacOS platforms
    Also fix test for Windows.
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -4,6 +4,10 @@ when 'darwin'
     its('stdout') { should match(/Mozilla Firefox /) }
     its('exit_status') { should eq 0 }
   end
+when 'windows'
+  describe registry_key 'HKLM\SOFTWARE\Mozilla\Mozilla Firefox' do
+    its('CurrentVersion') { should match /\d+\.\d+\.\d+ \(x64 en-US\)/ }
+  end
 else
   describe command 'firefox -v' do
     its('stdout') { should match(/Mozilla Firefox /) }
